0a31ef73c151153215000acf94e4a561c0718308
Gabriel Kunkel <henrysaltandson@gmail.com>
2019-12-11 12:00:05 -0600

    add component styling

--- src/ActiveCollection.tsx
+++ src/ActiveCollection.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import collection from "./Models/collection";
 import CardWrapper from "./CardWrapper";
 import NewCardForm from "./NewCardForm";
+import { makeStyles } from '@material-ui/core/styles'
 
 export interface IActiveCollectionProps {
     activeCollection: collection,
@@ -11,14 +12,24 @@ export interface IActiveCollectionProps {
     mergeNewCardFormObject: (obj: object) => void
 }
 
+const useStyles = makeStyles({
+  selectAddCard: {
+    textAlign: 'center',
+    cursor: 'pointer'
+  }
+});
+
 export default function ActiveCollection (props: IActiveCollectionProps) {  
+  const classes = useStyles();
+
   return (
     <div>
       <CardWrapper activeCollection={props.activeCollection}/>
+      <br />
       { props.showNewCardForm ? <NewCardForm 
         addACardToCurrentCollection={props.addACardToCurrentCollection}
         mergeNewCardFormObject={props.mergeNewCardFormObject}
-        /> : <div onClick={() => props.setShowNewCardForm(true)}>Add New Card To This Collection</div>}
+        /> : <div className={classes.selectAddCard} onClick={() => props.setShowNewCardForm(true)}>Add New Card To This Collection</div>}
     </div>
   );
 }
--- src/NewCardForm.tsx
+++ src/NewCardForm.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
 interface INewCardFormProps {
     addACardToCurrentCollection: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => boolean,
     mergeNewCardFormObject: (obj: object) => void
 }
 
-const NewCardForm: React.FunctionComponent<INewCardFormProps> = (props) => {
+const useStyles = makeStyles({
+  formWrapper: {
+    width: '80%',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  }
+});
 
-  return (<div>
-        <div><input id="word" placeholder="Vocabulary Word" onChange={e => props.mergeNewCardFormObject({word : e.target.value})}/></div>
-        <div><input id="definition" placeholder="Word Definition" onChange={e => props.mergeNewCardFormObject({definition : e.target.value})}/></div>
+
+const NewCardForm: React.FunctionComponent<INewCardFormProps> = (props) => {
+  const classes = useStyles();
+
+  return (<div className={classes.formWrapper}>
+        <div><TextField id="word" variant="outlined" placeholder="Vocabulary Word" onChange={e => props.mergeNewCardFormObject({word : e.target.value})}/></div>
+        <div><TextField id="definition" variant="outlined" placeholder="Word Definition" onChange={e => props.mergeNewCardFormObject({definition : e.target.value})}/></div>
         <button onClick={event => props.addACardToCurrentCollection(event)}>Add New Card</button>
   </div>);
 };
